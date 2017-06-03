a21bb2d6cb4834e7eb42e8673269414f4f7ec6ff
andela-akoya <adegboyega.koya@andela.com>
2017-06-03 23:54:10 +0100

    Revert "refactor show, update and delete action"
    This reverts commit 1fd727177cbfc7a5210dd5c356d10852efaa48d3.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-	before_action :set_article, only: [:edit, :update, :show, :destroy]
-
 	def index
 		@articles = Article.all
 	end
@@ -17,6 +15,7 @@ class ArticlesController < ApplicationController
 		end
 	end
 	def update
+		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:notice] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -24,18 +23,20 @@ class ArticlesController < ApplicationController
 			render 'edit'
 		end
 	end
-	def edit; end
-	def show; end
+	def edit
+		@article = Article.find(params[:id])
+	end
+	def show
+		@article = Article.find(params[:id])
+	end
 	def destroy
+		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:notice] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 
 	private
-	def set_article
-		@article = Article.find(params[:id])
-	end
 	def article_params
 		params.require(:article).permit(:title, :description)
 	end

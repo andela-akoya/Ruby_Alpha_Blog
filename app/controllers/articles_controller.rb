ac75c879c2b8ad6e950adf61405454c9baaba6f3
andela-akoya <adegboyega.koya@andela.com>
2017-06-03 23:54:10 +0100

    Revert "Modifies the implementaton of Article creation controller"
    This reverts commit a9fb86d8f68209586fb9f634572a9bceb2889b16.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,16 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 	def create
+		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		if @article.save
-			flash[:notice] = "Article was successfully created"
-			redirect_to article_path(@article)
-		else
-			render 'new'
-		end
-	end
-	def show
-		@article = Article.find(params[:id ])
+		@article.save
+		redirect_to articles_show(@article)
 	end
 
 	private

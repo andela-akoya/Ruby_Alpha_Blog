ad5bbfbbe4b7643d75b4db60a714cf3daa0f0415
andela-akoya <adegboyega.koya@andela.com>
2017-06-26 19:33:30 +0100

    Revert "Ft design form template"

--- app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ApplicationRecord
-    validates :title, presence: true, length: {minimum: 3, maximium: 50}
-    validates :description, presence: true, length: { minimum: 3, maximium: 300}
-
-end

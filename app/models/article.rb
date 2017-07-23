8b1bb3ab8e653934910408289bf3a182d3c3eae9
koya adegboyega <adegboyega.koya@andela.com>
2017-07-23 17:48:34 +0100

    create user table
    - add migration to create users table
    - add migration to add user_id column  in articles table
    - create seed functionality for user table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-	validates :title, presence: true, length: {minimum: 3, maximium: 50}
-	validates :description, presence: true, length: { minimum: 3, maximium: 300}
-	validates :image, presence: true
+  belongs_to :user
+  validates :title, presence: true, length: {minimum: 3, maximium: 50}
+  validates :description, presence: true, length: { minimum: 3, maximium: 300}
+  validates :image, presence: true
+  validates :user_id, :presence => true
 end
